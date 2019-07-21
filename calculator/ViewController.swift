feef9b5c71e22a2c3b8ae318e7411e604e8ca3ac
eratakumi <tera@coiney.com>
2019-07-21 19:24:23 +0900

    initialize project

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Takumi ERA on 2019/07/21.
+//  Copyright © 2019年 inmotion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
